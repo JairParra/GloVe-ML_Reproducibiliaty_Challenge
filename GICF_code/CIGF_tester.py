ec36adf07854b5e6f04ce391096e147b4afe751d
Jair <jair.parra@outlook.com>
2019-12-07 15:15:03 -0500

    updated

---
+++ GICF_code/CIGF_tester.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  7 14:16:57 2019
+
+@author: jairp
+"""
+
+### Tester for the CIGF related objects and classes 
+
+
+from util import io as cigf_io  # contains load_theta, save_theta functions   
+from util.data_handler import DataHandler # Data handler object
+
+
